eac7d92a38d1468f593bcb08538f3cb5643ce0db
Muhammad Khatri <khizar3840@gmail.com>
2018-11-26 18:26:59 -0600

    Fixed service worker

--- serviceworker.js
+++ serviceworker.js
@@ -25,8 +25,6 @@ const PRECACHE_URLS = [
   'secret.html',
   'sponsor.html',
   'sponsors.html',
-  './',
-  '.',// Alias for index.html
   'css/style.css',
   'css/animate.min.css',
   'css/font-awesome.min.css',
@@ -73,19 +71,8 @@ self.addEventListener('fetch', event => {
   // Skip cross-origin requests, like those for Google Analytics.
   if (event.request.url.startsWith(self.location.origin)) {
     event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
+      fetch(event.request).catch(function() {
+        return caches.match(event.request);
       })
     );
   }
