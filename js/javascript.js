ee186c68030243e59cbe336c40e9bd99e76bf4d2
Muhammad Khatri <khizar3840@gmail.com>
2018-11-27 23:15:05 -0600

    Fixed header scroll effect

--- js/javascript.js
+++ js/javascript.js
@@ -86,14 +86,31 @@ $('a[href*="#"]')
         event.preventDefault();
         $('html, body').animate({
           scrollTop: target.offset().top-50
-        }, 800, function() {
+        }, 700, function() {
 
         });
       }
     }
   });
-/*
+/*Scroll to top when arrow up clicked BEGIN*/
+$(window).scroll(function() {
+  var height = $(window).scrollTop();
+  if (height > 100) {
+    $('#back2Top').fadeIn();
+  } else {
+    $('#back2Top').fadeOut();
+  }
+});
+$(document).ready(function() {
+  $("#back2Top").click(function(event) {
+    event.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
+  });
 
+});
+/*Scroll to top when arrow up clicked END*/
+/*
 // Find all YouTube videos
 const $allVideos = $("iframe[src^='//www.youtube.com']"),
 
